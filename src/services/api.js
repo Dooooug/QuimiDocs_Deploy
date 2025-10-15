a814530789dafadef82672f59106af9dbe005e23
Dooooug <2225441@aluno.univesp.br>
2025-10-15 20:24:11 -0300

    Correcao package-json

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(
